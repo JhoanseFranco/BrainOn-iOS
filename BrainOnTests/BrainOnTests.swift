18672bbc874689123e3616f4423de08b31b25485
Jhoan Franco <jfranco@tribalworldwide.gt>
2025-12-10 14:36:58 -0500

    Initial commit: project structure MVVM and Brand colors setup

---
+++ BrainOnTests/BrainOnTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrainOnTests.swift
+//  BrainOnTests
+//
+//  Created by jhoan sebastian franco cardona on 10/12/25.
+//
+
+import Testing
+@testable import BrainOn
+
+struct BrainOnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
